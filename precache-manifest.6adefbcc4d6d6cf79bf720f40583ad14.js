581397e4df3a147e51207acd692254958f1535d8
Sentenzos <49534576+Sentenzos@users.noreply.github.com>
2020-02-18 19:49:38 +0300

    Updates

---
+++ precache-manifest.6adefbcc4d6d6cf79bf720f40583ad14.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3934b0748d79c32b51d16944e6af126",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "215745d76aef1654629f",
+    "url": "/landing/static/css/main.b4ffd2fa.chunk.css"
+  },
+  {
+    "revision": "5e4679be6f6229fed23d",
+    "url": "/landing/static/js/2.e8eebd99.chunk.js"
+  },
+  {
+    "revision": "af8d860248c3f8d68d3b584ef5cfb7e8",
+    "url": "/landing/static/js/2.e8eebd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215745d76aef1654629f",
+    "url": "/landing/static/js/main.5c207592.chunk.js"
+  },
+  {
+    "revision": "e360e12435fa3906baaf",
+    "url": "/landing/static/js/runtime-main.8e076927.js"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/landing/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/landing/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "46f0a3ad815962005753c02c4181aadc",
+    "url": "/landing/static/media/UbuntuMono-Bold.46f0a3ad.ttf"
+  },
+  {
+    "revision": "7da0dda7ca46200eebbc7b91d2cc94a9",
+    "url": "/landing/static/media/aboutImage.7da0dda7.jpg"
+  },
+  {
+    "revision": "44ee77199ee5bd482d851ff4afbea08f",
+    "url": "/landing/static/media/bubble.44ee7719.png"
+  },
+  {
+    "revision": "4d490a1265411c9b1535ce9b5c089306",
+    "url": "/landing/static/media/gothamBlack.4d490a12.otf"
+  },
+  {
+    "revision": "673854548dd0b65eca2f07306bf2364b",
+    "url": "/landing/static/media/laptop.67385454.png"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/landing/static/media/lato regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "d8d0e2ebd5bcbdfdc2899c9fc67eb5e7",
+    "url": "/landing/static/media/map.d8d0e2eb.png"
+  },
+  {
+    "revision": "46e94e7407740247cdadbf9875f1b5e2",
+    "url": "/landing/static/media/photo-1.46e94e74.png"
+  },
+  {
+    "revision": "fe48fa6f8adf878169fba1b0902c7705",
+    "url": "/landing/static/media/photo-2.fe48fa6f.png"
+  },
+  {
+    "revision": "5e0ea1efcd05bf20cd57c14aa0038576",
+    "url": "/landing/static/media/photo-3.5e0ea1ef.png"
+  },
+  {
+    "revision": "b45b23b2394e3033bb85ee01c5e672cc",
+    "url": "/landing/static/media/photo-4.b45b23b2.png"
+  }
+]);
